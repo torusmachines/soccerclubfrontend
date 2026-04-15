98f62fa4d543f68d788caa5b4e2707a92d7cb6fe
Jignesh Maru <jignesh.maru@torusitsolutions.com>
2026-04-15 12:23:37 +0530

    changed router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import { PlayerProvider } from "@/context/PlayerContext";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { PrivateRoute } from "@/components/PrivateRoute";
@@ -203,7 +203,7 @@ function RouterContent() {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <PlayerProvider>
             <Toaster />
@@ -211,7 +211,7 @@ const App = () => (
             <RouterContent />
           </PlayerProvider>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
